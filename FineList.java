2259c311ccbdf5f2fb51360e230fb78a716102c1
patrickkaleo <simuyembapatrick250@gmail.com>
2026-09-16 22:49:28 +0200

    Task 2 completed

--- FineList.java
+++ FineList.java
@@ -1,34 +1,101 @@
-public class FineList 
-{
+public class FineList {
 
     private final Node head;
     private final Node tail;
 
-    public FineList() 
-    {
+    public FineList() {
         head = new Node(Integer.MIN_VALUE);
         tail = new Node(Integer.MAX_VALUE);
 
         head.next = tail;
     }
 
-    public boolean add(int value) 
-    {
-        // TODO:
-        // Use hand-over-hand locking.
-        return false;
+    public boolean add(int value) {
+        this.head.lock.lock(); // lock head and traverse the list to add node
+        Node pred = this.head;
+        try {
+            Node curr = pred.next;
+            curr.lock.lock();
+            try {
+                while (curr.value < value) {
+                    pred.lock.unlock();
+                    pred = curr;
+                    curr = curr.next;
+                    curr.lock.lock();
+                }
+                if (value == curr.value)
+                    return false;
+
+                Node newNode = new Node(value);
+                newNode.next = curr;
+                pred.next = newNode;
+                return true;
+            } finally {
+                curr.lock.unlock();
+            }
+        } finally {
+            pred.lock.unlock();
+        }
     }
 
-    public boolean remove(int value) 
-    {
-        // TODO:
-        // Use hand-over-hand locking.
-        return false;
+    public boolean remove(int value) {
+        this.head.lock.lock();
+        Node pred = this.head;
+        try {
+            Node curr = pred.next;
+            curr.lock.lock();
+
+            try {
+                while (curr.value < value) {
+                    pred.lock.unlock();
+                    pred = curr;
+                    curr = curr.next;
+                    curr.lock.lock();
+                }
+
+                if (curr.value == value) {
+                    pred.next = curr.next;
+                    return true;
+                }
+
+                return false;
+            } finally {
+                curr.lock.unlock();
+            }
+
+        } finally {
+            pred.lock.unlock();
+        }
     }
 
-    public boolean contains(int value) 
-    {
-        // TODO
-        return false;
+    public boolean contains(int value) {
+        this.head.lock.lock();
+        if (value < this.head.value)
+            return false;
+
+        Node pred = this.head;
+        try {
+            Node curr = pred.next;
+            curr.lock.lock();
+            // traverse the list and find the node
+            try {
+                while (curr.value < value) {
+                    pred.lock.unlock();
+                    pred = curr;
+                    curr = curr.next;
+                    curr.lock.lock();
+                    if (curr.value == value)
+                        return true;
+                }
+                return false;
+
+            } finally {
+                curr.lock.unlock();
+            }
+
+        } finally {
+            pred.lock.unlock();
+        }
+
     }
 }
